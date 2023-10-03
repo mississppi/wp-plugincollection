55e65a0015717799b30705a5312975bc765dc906
mississppi <msspstudio@gmail.com>
2023-10-03 12:32:54 +0900

    フックをデバッグするプラグイン

--- view-hooks/create_option_page.php
+++ view-hooks/create_option_page.php
@@ -6,13 +6,20 @@ class CreateOptionPage
     {
         add_action('admin_menu', array($this, 'add_option_page'));
     }
-    
+
+    /**
+     * 管理メニューにオプションページを追加
+     */
     public function add_option_page()
     {
         $function = [$this, 'view_callback'];
         add_options_page('viewhook', 'viewhook', 'manage_options', 'view-hook', $function);
     }
 
+    /**
+     * オプションページのコールバック関数
+     * ビューファイルを読み込み
+     */
     public function view_callback()
     {
         include_once(plugin_dir_path(__FILE__) . 'view.php');
