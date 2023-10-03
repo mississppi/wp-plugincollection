9cd22961d3008aa5eb4116643e33b09c2c353438
mississppi <msspstudio@gmail.com>
2023-10-03 12:56:58 +0900

    コメントを追加

--- last-login-tracker/custom_user_columns.php
+++ last-login-tracker/custom_user_columns.php
@@ -4,20 +4,27 @@ class CustomUserColumns
 {
     public function __construct()
     {
-        //カラム追加
+        // カラムの追加と表示方法を設定するためのアクションフックとフィルターフックを登録
+        
+        // カラムを追加するフィルターフック
         add_filter('manage_users_columns', [$this, 'custom_add_last_login_column']);
+        
+        // カラムの内容を表示するアクションフック
         add_action('manage_users_custom_column', [$this, 'custom_display_last_login_column'], 10, 3);
 
     }
 
     public function custom_add_last_login_column($columns)
     {
+        // ユーザー一覧のカラムに「最終ログイン日時」を追加
         $columns['last_login'] = '最終ログイン日時';
         return $columns;
     }
 
     public function custom_display_last_login_column($value, $column_name, $user_id)
     {
+        // 最終ログイン日時カラムの内容を表示
+
         if($column_name === 'last_login'){
             $last_login = get_user_meta($user_id, 'last_login', true);
             if($last_login){
--- last-login-tracker/update_last_login.php
+++ last-login-tracker/update_last_login.php
@@ -4,14 +4,16 @@ class updateLastLogin
 {
     public function __construct()
     {
+        // ユーザーがログインしたときに最終ログイン日時を更新するためのアクションフックを登録
         add_action('wp_login', [$this, 'custom_update_last_login'], 10, 2);
     }
 
     public function custom_update_last_login($user_login, $user) {
+        // ユーザーの最終ログイン日時を更新するカスタム関数
+
         // ユーザーの最終ログイン日時を更新
         update_user_meta($user->ID, 'last_login', current_time('timestamp'));
     }
 }
 
-new updateLastLogin();
-
+new updateLastLogin(); // updateLastLoginクラスのインスタンスを作成し、ユーザーがログインするたびに最終ログイン日時を更新します。
